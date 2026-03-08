8483eb7e4c6178db9a9ffd52399db885e5e5f8d4
i567283 <radoslav.dodnikov@sap.com>
2026-03-08 18:18:17 +0200

    chore(seo): Add sitemap, robots.txt, and JSON-LD structural data

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import node from '@astrojs/node';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://siteintelica.com',
+  integrations: [sitemap()],
   adapter: node({
     mode: 'standalone'
   })
